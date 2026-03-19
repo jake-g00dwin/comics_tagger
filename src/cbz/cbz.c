f86245a00cfe6a0a0d2cbc1728131997a87b033c
jake-g00dwin <jakegoodwin@tutanota.com>
2026-03-19 00:34:41 -0700

    Added C based module "CBZ" to soruces.
    This module is designed to abstract the concept of an individual comic
    book archive file.

---
+++ src/cbz/cbz.c
@@ -0,0 +1,16 @@
+/*
+ * Author: username
+ * Date: 2026
+ * filename: cbz.c
+ * description: module_purpose
+ */
+
+#include "cbz.h"
+
+// dumb test function
+int add_two(int a)
+{
+    int b = a;
+    b += 2;
+    return b;
+}
---
+++ src/cbz/cbz.h
@@ -0,0 +1,29 @@
+/**
+ * @brief Module description
+ * @details This file is an <Purpose here>
+ * @author Jake G
+ * @date 2026
+ * @copyright None
+ * @file cbz.h
+ */
+
+// #pragma once
+#ifndef cbz_H
+#define cbz_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+    int add_two(int a);
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // cbz_H
