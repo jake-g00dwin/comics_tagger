f16be0f205cd2ffcf93fbc297604eed5ca74c540
jake-g00dwin <jakegoodwin@tutanota.com>
2026-03-19 20:57:26 -0700

    now linking and building with libzip.

--- src/cbz/cbz.c
+++ src/cbz/cbz.c
@@ -6,7 +6,47 @@
  */
 
 #include "cbz.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/syscall.h>
+#include <errno.h>
+
+//#include <sys/stat.h>
+
+//#include <locale.h>
+//#include <uchar.h>
+//#include <wchar.h>
+
 #include <zip.h>
+
+
+int cbz_init_metadata(cbz_metadata_t *md)
+{
+    md = (cbz_metadata_t *)malloc(sizeof(cbz_metadata_t));
+    if(!md){
+        fprintf(stderr, "Failed to allocate memory: cbz_init_metadata()\n");
+        return STATUS_ERR;
+    }
+
+    return STATUS_OK;
+}
+
+int  cbz_create(cbz_t *cbz, const char *path)
+{
+    zip_t *archive;
+    zip_error_t *zip_err;
+    zip_source_t *zip_src;
+
+    int error;
+
+    //Create the archive if it doesn't exist, throw error if it does.
+    archive = zip_open(path, ZIP_CREATE|ZIP_EXCL, &error);
+    //archive = zip_open_from_source(zip_src,ZIP_CREATE|ZIP_EXCL , zip_err);
+
+    return 0;
+}
 
 // dumb test function
 int add_two(int a)
--- src/cbz/cbz.h
+++ src/cbz/cbz.h
@@ -19,8 +19,69 @@ extern "C"
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 
-    int add_two(int a);
+enum status_codes{
+    STATUS_OK = 0,
+    STATUS_ERR = 1,
+};
+
+    typedef struct{
+        int year;
+        int month;
+        int day;
+    }pub_date_t;
+
+    typedef struct{
+        char *name;
+        char *value;
+    }metadata_field_t;
+
+    typedef struct{
+        char *tag_type;
+        char *tag_value;
+    }metadata_tag_t;
+
+    typedef struct
+    {
+        char *title;
+        char *language;
+        char *description;
+/*
+        char *source_url;
+        bool is_multi_work;
+        bool is_manga;
+        bool is_colored;
+        int version;
+        uint16_t view_count;
+        uint8_t stars;
+        pub_date_t published_date;
+        size_t bookmarked_page_index;
+        size_t num_pages;
+*/
+    }cbz_metadata_t;
+
+
+
+    typedef struct 
+    {
+        cbz_metadata_t meta_data;
+        char  *filename;
+        char *file_path;
+    }cbz_t;
+
+    //cbz_metadata_t cbz_init_metadata(void);
+    int cbz_init_metadata(cbz_metadata_t *md);
+
+    cbz_t cbz_new(void);
+    int    cbz_create(cbz_t *cbz);
+    int    cbz_write(void);
+    int    cbz_open(void);
+    int    cbz_get_metadata(void);
+    int    cbz_set_metadata(void);
+    int    cbz_get_image(size_t page_index);
+    size_t cbz_get_size(void);
+    int get_cbz_data(void);
 
 #ifdef __cplusplus
 }
