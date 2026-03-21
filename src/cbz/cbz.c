9f9d788bfea23b6ee10d93698edb334d90d76011
jake-g00dwin <jakegoodwin@tutanota.com>
2026-03-20 23:04:01 -0700

    Wrote passing code to metadata init/free operations.

--- src/cbz/cbz.c
+++ src/cbz/cbz.c
@@ -10,40 +10,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <sys/syscall.h>
 #include <errno.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
 
-//#include <sys/stat.h>
-
-//#include <locale.h>
-//#include <uchar.h>
-//#include <wchar.h>
+// #include <uchar.h>
 
 #include <zip.h>
 
-
 int cbz_init_metadata(cbz_metadata_t *md)
 {
-    md = (cbz_metadata_t *)malloc(sizeof(cbz_metadata_t));
-    if(!md){
-        fprintf(stderr, "Failed to allocate memory: cbz_init_metadata()\n");
+    if (md->is_initialized)
+    {
+        fprintf(stderr, "Passed metadata already initialized.\n");
         return STATUS_ERR;
     }
+
+    md->title = calloc(CBZ_MAX_TITLE_LEN, sizeof(char));
+    if (!md->title)
+    {
+        fprintf(stderr, "Failed to allocate memory for: title\n");
+        return STATUS_ERR;
+    }
+    strcpy(md->title, "default_title");
+
+    md->series = calloc(CBZ_DEFAULT_FIELD_LEN, sizeof(char));
+    if (!md->series)
+    {
+        fprintf(stderr, "Failed to allocate memory for: series\n");
+        return STATUS_ERR;
+    }
+
+    md->author = calloc(CBZ_MAX_AUTHOR_LEN, sizeof(char));
+    if (!md->author)
+    {
+        fprintf(stderr, "Failed to allocate memory for: author\n");
+        return STATUS_ERR;
+    }
+
+    md->publisher = calloc(CBZ_DEFAULT_FIELD_LEN, sizeof(char));
+    if (!md->publisher)
+    {
+        fprintf(stderr, "Failed to allocate memory for: publisher\n");
+        return STATUS_ERR;
+    }
+
+    md->year           = (uint16_t)CBZ_DEFAULT_YEAR;
+    md->month          = (uint8_t)CBZ_DEFAULT_MONTH;
+    md->day            = (uint8_t)CBZ_DEFAULT_DAY;
+
+    md->is_initialized = true;
+    return STATUS_OK;
+}
+
+int cbz_free_metadata(cbz_metadata_t *md)
+{
+    if (!md->is_initialized)
+    {
+        fprintf(stderr, "Error: passed uninitialized struct.\n");
+        return STATUS_ERR;
+    }
+
+    free(md->title);
+    md->title = NULL;
+
+    free(md->series);
+    md->series = NULL;
+
+    free(md->author);
+    md->author = NULL;
+
+    free(md->publisher);
+    md->publisher = NULL;
+
+    md->year      = (uint16_t)CBZ_DEFAULT_YEAR;
+    md->month     = (uint8_t)CBZ_DEFAULT_MONTH;
+    md->day       = (uint8_t)CBZ_DEFAULT_DAY;
 
     return STATUS_OK;
 }
 
-int  cbz_create(cbz_t *cbz, const char *path)
+int cbz_create(cbz_t *cbz, const char *path)
 {
-    zip_t *archive;
-    zip_error_t *zip_err;
+    zip_t        *archive;
+    zip_error_t  *zip_err;
     zip_source_t *zip_src;
 
-    int error;
+    int           error;
 
-    //Create the archive if it doesn't exist, throw error if it does.
-    archive = zip_open(path, ZIP_CREATE|ZIP_EXCL, &error);
-    //archive = zip_open_from_source(zip_src,ZIP_CREATE|ZIP_EXCL , zip_err);
+    // Create the archive if it doesn't exist, throw error if it does.
+    archive = zip_open(path, ZIP_CREATE | ZIP_EXCL, &error);
+    // archive = zip_open_from_source(zip_src,ZIP_CREATE|ZIP_EXCL , zip_err);
 
     return 0;
 }
--- src/cbz/cbz.h
+++ src/cbz/cbz.h
@@ -16,63 +16,102 @@ extern "C"
 {
 #endif
 
+#include <locale.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <wchar.h>
+#include <zip.h>
 
-enum status_codes{
-    STATUS_OK = 0,
-    STATUS_ERR = 1,
-};
+#define CBZ_MAX_TITLE_LEN 255
+#define CBZ_MAX_AUTHOR_LEN 255
+#define CBZ_MAX_LANG_LEN 32
+#define CBZ_MAX_DESC_LEN 512
+#define CBZ_DEFAULT_FIELD_LEN 255
 
-    typedef struct{
-        int year;
-        int month;
-        int day;
-    }pub_date_t;
+#define CBZ_DEFAULT_YEAR 1971
+#define CBZ_DEFAULT_MONTH 1
+#define CBZ_DEFAULT_DAY 1
 
-    typedef struct{
-        char *name;
-        char *value;
-    }metadata_field_t;
-
-    typedef struct{
-        char *tag_type;
-        char *tag_value;
-    }metadata_tag_t;
+    enum status_codes
+    {
+        STATUS_OK  = 0,
+        STATUS_ERR = 1,
+    };
 
     typedef struct
     {
-        char *title;
-        char *language;
-        char *description;
-/*
-        char *source_url;
-        bool is_multi_work;
-        bool is_manga;
-        bool is_colored;
-        int version;
-        uint16_t view_count;
-        uint8_t stars;
-        pub_date_t published_date;
-        size_t bookmarked_page_index;
-        size_t num_pages;
-*/
-    }cbz_metadata_t;
+        char        *name;
+        zip_uint64_t index;
+    } cbz_page_t;
 
-
-
-    typedef struct 
+    typedef struct
     {
+        int year;
+        int month;
+        int day;
+    } pub_date_t;
+
+    typedef struct
+    {
+        char *name;
+        char *value;
+    } metadata_field_t;
+
+    typedef struct
+    {
+        metadata_field_t *fields;
+        size_t            length;
+        size_t            vec_capacity;
+    } metadata_field_vec_t;
+
+    typedef struct
+    {
+        char *tag_type;
+        char *tag_value;
+    } metadata_tag_t;
+
+    typedef struct
+    {
+        metadata_tag_t *tags;
+        size_t          length;
+        size_t          vec_capacity;
+    } metadata_tag_vec_t;
+
+    typedef struct
+    {
+        char              *title;
+        char              *series;
+        char              *author;
+        char              *publisher;
+        uint16_t           year;
+        uint8_t            month;
+        uint8_t            day;
+        uint16_t           issue;
+        char              *language;
+        char              *description;
+        metadata_tag_vec_t tag_vector;
+        bool               is_initialized;
+    } cbz_metadata_t;
+
+    typedef struct
+    {
+        zip_t         *archive;
+        cbz_page_t    *pages;
+        size_t         page_count;
+        size_t         capacity;
         cbz_metadata_t meta_data;
-        char  *filename;
-        char *file_path;
-    }cbz_t;
+        char          *filename;
+        char          *file_path;
+        bool           has_metadata;
+    } cbz_t;
 
-    //cbz_metadata_t cbz_init_metadata(void);
+    // cbz_metadata_t cbz_init_metadata(void);
     int cbz_init_metadata(cbz_metadata_t *md);
+    int cbz_free_metadata(cbz_metadata_t *md);
 
+    /*
     cbz_t cbz_new(void);
     int    cbz_create(cbz_t *cbz);
     int    cbz_write(void);
@@ -82,6 +121,7 @@ enum status_codes{
     int    cbz_get_image(size_t page_index);
     size_t cbz_get_size(void);
     int get_cbz_data(void);
+    */
 
 #ifdef __cplusplus
 }
