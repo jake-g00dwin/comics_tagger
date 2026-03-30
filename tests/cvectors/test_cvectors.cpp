a7297877d3babd2cb2fe2a9efbee56d1c8b09cac
jake-g00dwin <jakegoodwin@tutanota.com>
2026-03-29 18:27:45 -0700

    Updated cvector test suite
    + Added new memory allocation fail settings.
    + Added new test for alloc memory allocatio failures.

--- tests/cvectors/test_cvectors.cpp
+++ tests/cvectors/test_cvectors.cpp
@@ -16,6 +16,8 @@ extern "C"
 static size_t calloc_count = 0;
 static size_t malloc_count = 0;
 static size_t free_count = 0;
+static int fail_after = -1;
+static int fail_after_count = 0; 
 
 static void *calloc_impl(size_t number, size_t size);
 static void *malloc_impl(size_t size);
@@ -31,6 +33,8 @@ TEST_GROUP(tg_cvectors)
         calloc_count = 0;
         malloc_count = 0;
         free_count = 0;
+        fail_after = -1;
+        fail_after_count = 0;
     }
     void teardown()
     {
@@ -40,12 +44,29 @@ TEST_GROUP(tg_cvectors)
 
 void *calloc_impl(size_t number, size_t size)
 {
+    if(fail_after >= 0)
+    {
+        fail_after_count += 1;
+        if(fail_after <= fail_after_count){
+            return NULL;
+        }
+    }
+
     calloc_count += 1;
     return calloc(number, size);
 }
 
 void *malloc_impl(size_t size)
 {
+
+    if(fail_after >= 0)
+    {
+        fail_after_count += 1;
+        if(fail_after <= fail_after_count){
+            return NULL;
+        }
+    }
+
     malloc_count += 1;
     return malloc(size);
 }
@@ -76,16 +97,30 @@ TEST(tg_cvectors, cvec_capacityFailsOnNull)
 TEST(tg_cvectors, createfailsonzerosize)
 {
     size_t element_size = 0;
-    cvec_t *vec_ptr = cvec_create(element_size);
+    result_cvec_pt rcv = cvec_create(element_size);
 
-    CHECK_EQUAL_TEXT(NULL, vec_ptr,"The vector ptr should be null on create failure.");
+    CHECK_FALSE_TEXT(rcv.is_ok, "The vector ptr should be null on create failure.");
+    CHECK_TRUE(rcv.value != NULL);
+}
+
+TEST(tg_cvectors, createHandlesFailedAlloc)
+{
+    fail_after = 0; //Causes malloc call to fail on first call.
+    
+    result_cvec_pt rvec = cvec_create(sizeof(uint8_t));
+
+    CHECK_FALSE(rvec.is_ok);
+    CHECK_EQUAL(status_std_alloc_failure, rvec.error);
+
 }
 
 TEST(tg_cvectors, createallocatesmemory)
 {
     result_int_t ri;
     result_size_t rs;
-    cvec_t *vec_u8_ptr = cvec_create(sizeof(uint8_t));
+    result_cvec_pt rcv = cvec_create(sizeof(uint8_t));
+    cvec_t *vec_u8_ptr = rcv.value; 
+
     if(!vec_u8_ptr){
         FAIL("Returned vector is NULL");
     }
@@ -100,7 +135,6 @@ TEST(tg_cvectors, createallocatesmemory)
             "Initialized with incorrect capacity.");
     rs = cvec_size(vec_u8_ptr);
     CHECK_EQUAL_TEXT(0, rs.value, "Initialized with incorrect size.");
-
 
     ri = cvec_destroy(vec_u8_ptr);
     CHECK_TRUE_TEXT(ri.is_ok, "Result of cvec_destroy() is not ok.");
@@ -117,7 +151,9 @@ TEST(tg_cvectors, getElementFailsOnNull)
 
 TEST(tg_cvectors, getElementFailsOnInvalidIndexLessThan)
 {
-    cvec_t *vec = cvec_create(sizeof(uint8_t));
+    result_cvec_pt rcv = cvec_create(sizeof(uint8_t));
+    cvec_t *vec = rcv.value;
+    
     result_void_pt rvp = cvec_get(vec, 0);
     
     CHECK_FALSE(rvp.is_ok);
@@ -130,14 +166,21 @@ TEST(tg_cvectors, appendFailsOnNullVec)
 {
     cvec_t *vec = NULL;
     uint8_t value = 255;
-    CHECK_FALSE(cvec_append(vec, &value).is_ok);
+    result_int_t ri = cvec_append(vec, &value);
+
+    CHECK_FALSE(ri.is_ok);
+    CHECK_EQUAL(status_std_null_ptr, ri.error);
 }
 
 TEST(tg_cvectors, appendFailsOnNullData)
 {
-    cvec_t *vec = cvec_create(sizeof(uint8_t));
+    result_cvec_pt rcv = cvec_create(sizeof(uint8_t));
+    cvec_t *vec = rcv.value;
+    result_int_t ri = cvec_append(vec, NULL);
 
-    CHECK_FALSE(cvec_append(vec, NULL).is_ok);
+    CHECK_FALSE(ri.is_ok);
+    CHECK_EQUAL(status_std_invalid_arg, ri.error);
+
     CHECK_TRUE(cvec_destroy(vec).is_ok);
 }
 
@@ -145,10 +188,14 @@ TEST(tg_cvectors, appendGetOkOnValidVec)
 {
     result_size_t rsz;
     result_void_pt rvp;
+    result_int_t ri;
 
-    cvec_t *vec = cvec_create(sizeof(uint8_t));
+    result_cvec_pt rcv = cvec_create(sizeof(uint8_t));
+    cvec_t *vec = rcv.value;
     uint8_t value = 255;
-    CHECK_TRUE(cvec_append(vec, &value).is_ok);
+
+    ri = cvec_append(vec, &value);
+    CHECK_TRUE(ri.is_ok);
     
     rsz = cvec_size(vec);
     CHECK_EQUAL(1, rsz.value); 
@@ -166,7 +213,10 @@ TEST(tg_cvectors, appendGetGrowsCapacity)
 {
     result_size_t rsize;
     result_size_t rcap;
-    cvec_t *vec = cvec_create(sizeof(uint8_t));
+
+    result_cvec_pt rcv = cvec_create(sizeof(uint8_t));
+    cvec_t *vec = rcv.value;
+
     size_t calloc_count_init = calloc_count;
     size_t free_count_init = free_count;
 
@@ -202,6 +252,7 @@ TEST(tg_cvectors, popFailsOnNullVector)
 TEST_GROUP(tg_cvec_populated)
 {
     const size_t prealloced_elements = 16;
+    result_cvec_pt rcv;
     cvec_t *vec_ptr; 
     void setup()
     {
@@ -209,8 +260,9 @@ TEST_GROUP(tg_cvec_populated)
         UT_PTR_SET(malloc_fn, malloc_impl);
         UT_PTR_SET(free_fn, free_impl);
 
-        vec_ptr = cvec_create(sizeof(uint8_t));
-        CHECK_TRUE(vec_ptr != NULL);
+        rcv = cvec_create(sizeof(uint8_t));
+        CHECK_TRUE(rcv.is_ok);
+        vec_ptr = rcv.value;
 
         for(uint8_t i = 0; i < prealloced_elements; i++){
             cvec_append(vec_ptr, &i);
@@ -226,6 +278,25 @@ TEST_GROUP(tg_cvec_populated)
         CHECK_TRUE(cvec_destroy(vec_ptr).is_ok);
     }
 };
+
+TEST(tg_cvec_populated, appendHandlesFailedAlloc)
+{
+    //result_size_t vec_size = cvec_size(vec_ptr);
+    //result_size_t vec_size = cvec_size(vec_ptr);
+    //size_t vec_size = cvec_size(vec_ptr).value;
+    size_t vec_cap = cvec_capacity(vec_ptr).value;
+    uint8_t u8val = 128;
+    fail_after = 0;
+
+    //Append until it will attempts to allocate memory.
+    while(cvec_size(vec_ptr).value < vec_cap - 1){
+        CHECK_TRUE(cvec_append(vec_ptr, &u8val).is_ok);
+    }
+    
+    result_int_t ri = cvec_append(vec_ptr, &u8val);
+    CHECK_FALSE(ri.is_ok);
+    CHECK_EQUAL(status_std_alloc_failure, ri.error);
+}
 
 TEST(tg_cvec_populated, popShrinksVectorCapacity)
 {
